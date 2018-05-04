e5c4af555494ff2246160f46f4870f4521486add
dvega03 <dvega03@ucm.es>
2018-05-04 21:36:41 +0200

    La energia se destruye y da energia

---
+++ Assets/Scripts/EnergiaBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergiaBehaviour : MonoBehaviour {
+
+    Collider2D colEner;
+    EnergyManager energyManager;
+
+
+	void Start ()
+    {
+        colEner = GetComponent<Collider2D>();
+	}
+
+   
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "JugadorLuz")
+        {
+            energyManager.SumaEnergia();
+            Destroy(gameObject);
+        }
+    }
+
+}
