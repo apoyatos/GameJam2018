b18678ef3c72510c849dd973358e89bed5188c13
pablojor <pablojor@ucm.es>
2018-05-04 20:14:27 +0200

    EnergyManager

---
+++ Assets/Scripts/EnergyManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergyManager : MonoBehaviour {
+
+    public int energiaInicial =10;
+
+    int actual;
+
+	void awake () {
+        actual = energiaInicial;
+	}
+
+
+    public void RestaEnergia()
+    {
+        actual--;
+    }
+    public void SumaEnergia()
+    {
+        actual++;
+    }
+    public int EnergiaActual()
+    {
+        return actual;
+    }
+}
