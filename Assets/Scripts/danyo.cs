5f9352910c0d59f9a005f7063b1eb68a9ce97eaf
apoyatos <37510165+apoyatos@users.noreply.github.com>
2018-05-04 21:33:36 +0200

    Añadido el daño al tocar la roca

---
+++ Assets/Scripts/danyo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class danyo : MonoBehaviour {
+    EnergyManager energyManager;
+    private void Start()
+    {
+        energyManager = FindObjectOfType<EnergyManager>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Roca")
+            energyManager.RestaEnergia();
+    }
+}
