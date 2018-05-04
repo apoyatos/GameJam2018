57c2f7ecacaea10e501959a690b2caffe8de4a6b
dvega03 <dvega03@ucm.es>
2018-05-04 21:20:58 +0200

    Plataforma nube

---
+++ Assets/PlatNube.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatNube : MonoBehaviour {
+
+
+    
+    public float tempDestruccion = 3f;
+
+    SpriteRenderer sprt;
+    Color alpha;
+
+	// Use this for initialization
+	void Start ()
+    {
+        alpha = sprt.GetComponent<SpriteRenderer>().color;
+        sprt = GetComponent<SpriteRenderer>();
+	}
+	
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.collider.tag == "JugadorLuz")
+        {
+            Reducir();
+        }
+    }
+
+    void Reducir()
+    {
+        
+        alpha.a--;
+
+        if(alpha.a <= 0)
+        {
+            Destroy(gameObject);
+        }else
+        {
+            Invoke("Reducir", tempDestruccion / 255);
+        }
+    }
+
+   
+}
---
+++ Assets/PlatNubeDetecta.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatNubeDetecta: MonoBehaviour {
+
+    bool Atravesado;
+    Collider2D colisionPlataforma;
+    Collider2D colisionActivacol;
+
+	void Start ()
+    {
+        colisionPlataforma = GetComponentInParent<Collider2D>();
+        colisionActivacol = GetComponent<Collider2D>();
+        colisionPlataforma.enabled = true;
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "JugadorLuz")
+        {
+            colisionPlataforma.enabled = false;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.tag == "JugadorLuz")
+        {
+            colisionPlataforma.enabled = true;
+        }
+    }
+
+  
+    
+
+    
+
+    
+
+    
+}
