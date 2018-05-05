9e47974203540b8c1f500be6e27a9824bcf70738
pablojor <pablojor@ucm.es>
2018-05-05 12:04:22 +0200

    Animacion Sombra

---
+++ Assets/Scripts/AnimacionSombra.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimacionSombra : MonoBehaviour {
+
+    Animator anim;
+    Rigidbody2D rb2D;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (rb2D.velocity.y > 0.5f)
+            anim.SetInteger("Direction", 1);
+
+        else if (rb2D.velocity.y < -0.5f)
+            anim.SetInteger("Direction", 2);
+
+        else if (rb2D.velocity.x > 0.5f)
+            anim.SetInteger("Direction", 3);
+        else if (rb2D.velocity.x < -0.5f)
+            anim.SetInteger("Direction", 4);
+
+    }
+}
