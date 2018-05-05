8526c464280ffb7579d0bc776546f661f8efd896
apoyatos <37510165+apoyatos@users.noreply.github.com>
2018-05-05 11:30:28 +0200

    arregladas las nubes

--- Assets/Scripts/PlatNubeDetecta.cs
+++ Assets/Scripts/PlatNubeDetecta.cs
@@ -16,9 +16,9 @@ public class PlatNubeDetecta: MonoBehaviour {
         colisionPlataforma.enabled = true;
     }
 
-    void OnTriggerStay2D(Collider2D other)
+    void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Luz")
+        if (other.tag == "Cabeza")
         {
             colisionPlataforma.enabled = false;
         }
@@ -26,7 +26,7 @@ public class PlatNubeDetecta: MonoBehaviour {
 
     void OnTriggerExit2D(Collider2D other)
     {
-        if(other.tag == "Luz")
+        if(other.tag == "Pies")
         {
             colisionPlataforma.enabled = true;
         }
