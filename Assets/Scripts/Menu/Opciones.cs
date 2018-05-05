3f51538def7506b860e2f23fa890adc7859ff212
apoyatos <37510165+apoyatos@users.noreply.github.com>
2018-05-05 08:44:14 +0200

    Menu de Opciones

---
+++ Assets/Scripts/Menu/Opciones.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Opciones : MonoBehaviour {
+
+    public GameObject botones;
+    public GameObject panelOpciones;
+    //Métodos para el panel de opciones
+    public void AbreOpciones()
+    {
+        botones.SetActive(false);
+        panelOpciones.gameObject.SetActive(true);
+    }
+    public void CierraOpciones()
+    {
+        botones.SetActive(true);
+        panelOpciones.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Menu/SettingsManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingsManager : MonoBehaviour {
+
+	public AudioMixer mainMixer;
+	public Dropdown listaCalidad, listaResoluciones;
+
+	Resolution[] resoliciones;
+	void Start(){
+		resoliciones = Screen.resolutions;
+		listaResoluciones.ClearOptions ();
+		List<string> opciones = new List<string>();
+		int indiceResolucionActual = 0;
+		for (int i = 0; i < resoliciones.Length; i++) {
+			string option = resoliciones[i].width + " x " + resoliciones[i].height;
+			opciones.Add(option);
+
+			if (resoliciones [i].width == Screen.currentResolution.width &&
+			   resoliciones [i].height == Screen.currentResolution.height)
+				indiceResolucionActual = i;
+		}
+
+		listaResoluciones.AddOptions (opciones);
+		listaResoluciones.value = indiceResolucionActual;
+		listaResoluciones.RefreshShownValue ();
+		CambiaGraficos (QualitySettings.GetQualityLevel ());
+
+	}
+
+	//Métodos para el apartado de opciones
+	public void CambiaVolumen(float volume){
+		mainMixer.SetFloat ("volume", volume);
+	}
+
+	public void CambiaGraficos(int index){
+		listaCalidad.value = index;
+		listaCalidad.RefreshShownValue ();
+		QualitySettings.SetQualityLevel (index);
+	}
+
+	public void CambiaAPantallaCompleta(bool pantCompleta){
+		Screen.fullScreen = pantCompleta;
+	}
+
+	public void ActualizaResolucion(int indice){
+		Resolution res = resoliciones [indice];
+		Screen.SetResolution (res.width, res.height, Screen.fullScreen);
+	}
+}
